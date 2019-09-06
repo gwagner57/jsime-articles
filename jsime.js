cfc546596796a7d8c5120d0f962d1943828583a4
Luis Gustavo Nardin <gnardin@gmail.com>
2019-09-06 16:52:25 +0200

    Add social media icons

---
+++ jsime.js
@@ -0,0 +1,54 @@
+var shareButton = document.getElementById( "share-this" );
+var shareLinkList = document.querySelector( "ul.links" );
+var canonicalUrlLinkElem = document.querySelector( "link[rel=canonical]" );
+var pageUrl = canonicalUrlLinkElem ? canonicalUrlLinkElem.href : document.URL;
+var title = document.querySelector( "meta[property='og:title']" )
+  .getAttribute( "content" );
+var description = document.querySelector( "meta[property='og:description']" )
+  .getAttribute( "content" );
+
+/** */
+function setShareLinks() {
+  var emailLinkEl = document.querySelector( ".share.email > a" );
+  var elements = document.querySelectorAll( ".share.facebook" );
+  Array.prototype.forEach.call( elements, function ( el ) {
+    el.addEventListener( "click", function () {
+      var url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
+      window.open( url, "NewWindow", "" );
+    } );
+  } );
+  elements = document.querySelectorAll( ".share.twitter" );
+  Array.prototype.forEach.call( elements, function ( el ) {
+    el.addEventListener( "click", function () {
+      var url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" +
+        title;
+      window.open( url, "NewWindow" );
+    } );
+  } );
+  elements = document.querySelectorAll( ".share.linkedin" );
+  Array.prototype.forEach.call( elements, function ( el ) {
+    el.addEventListener( "click", function () {
+      var url = "https://www.linkedin.com/shareArticle?mini=true&url=" +
+        pageUrl;
+      window.open( url, "NewWindow" );
+    } );
+  } );
+  emailLinkEl.href = "mailto:?subject=" + title + "&body=" + description +
+    "%0A%0A" + pageUrl;
+}
+if ( navigator.share ) {
+  shareButton.style.display = "inline"
+  shareButton.addEventListener( "click", function () {
+    navigator.share( {
+      title: title,
+      url: pageUrl,
+      text: description
+    } ).then( function () {
+      console.log( "Thanks for sharing!" );
+    } )
+      .catch( console.error );
+  } );
+} else {
+  shareLinkList.style.display = "inline-block";
+  setShareLinks();
+}
